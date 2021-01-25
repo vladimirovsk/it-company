b5d621954b91d07eb9e8d3d2fd759d4ce37b7eb6
vladimirovsk <vladimirovsk@gmail.com>
2021-01-25 22:40:08 +0100

    master

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import {Container, Col, Row} from 'react-bootstrap';
 import './About.css'
 
+import vsc from '../img/vsc1.jpg'
+import db from '../img/DatabaseDesign2.jpg'
 
 
 function About(){
   return(
       <Container id='about' fluid={true} className={'About'}>
-        <h2>About</h2>
+       <Row >
+          <Col className='Grid' md={12} lg={6} style={{        
+                backgroundImage: `url(${db})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'}}>
+         
+          </Col>
+          <Col className='Grid' md={12} lg={6} style={{backgroundColor:'whitesmoke'}}></Col>
+        </Row>  
+        <Row >
+          <Col className='Grid' md={12} lg={6} style={{backgroundColor:'whitesmoke'}}></Col>
+          <Col className='Grid' md={12} lg={6} style={{        
+                backgroundImage: `url(${vsc})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'}}>
+         
+          </Col>
+        </Row>  
       </Container>
   )
 }
--- src/component/project/Project.js
+++ src/component/project/Project.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
+
+
+
 import './Project.css'
+
 
 
 
 function Project(){
   return(
       <Container id='project' fluid={true} className={'Project'}>
-        <h2>Project</h2>
+        <h1>Project</h1>
       </Container>
   )
 }
